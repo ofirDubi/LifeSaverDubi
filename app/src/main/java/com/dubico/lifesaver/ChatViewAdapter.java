730f4c3a67b85217ffae852e8e8885d9b5c3131d
ofirDubi <ofir123dubi@gmail.com>
2016-09-12 12:34:50 +0300

    First commit

---
+++ app/src/main/java/com/dubico/lifesaver/ChatViewAdapter.java
@@ -0,0 +1,14 @@
+package com.dubico.lifesaver;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Asus on 25/08/2016.
+ */
+public class ChatViewAdapter extends ArrayAdapter<String> {
+
+    public ChatViewAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+}
